630da97ce9f59586fe99a7a9e94d6f00d254255c
U-asus-tasty\tastyminerals <tastyminerals@gmail.com>
2015-07-14 18:39:34 +0200

    added main stub and helped json file

--- chatbotty.py
+++ chatbotty.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""Small chatbot demo using aiml rules"""
+"""
+Small chatbot demo using aiml rules
+"""
 
 import sys
 import os
 sys.path.append(os.path.join(os.getcwd(), r'pyaiml/pyaiml3-master'))
 import aiml
+import json
+
+class Chatbotty_helper:
+    def __init__(self, path):
+        self.path_to_brains = path
+        self._prepare_brains()
+    def _prepare_brains(self):
+        with open(self.path_to_brains, 'r') as jfile:
+            brains = json.load(jfile)
+        print(brains)
 
 class Chatbotty:
     def __init__(self):
         self.chatbot = aiml.Kernel()
-    def get_ideas(self, aiml_file):
+    def include(self, aiml_file):
         self.chatbot.learn(aiml_file)
-    def respond(self, user_input):
+    def entertain(self, user_input):
         print(self.chatbot.respond(user_input))
 
 
-if __name__ == '__main__':
+def wake_the_bot():
+    """
+    This function is a bot chat session initializer.
+    """
     bot = Chatbotty()
-    bot.get_ideas(sys.argv[1])
+    bot.include('cocktail_brains.aiml')
+    help_bot = Chatbotty_helper('small_brains.json')
+    
     while True:
-        i = input('>')
-        print(i)
-        bot.respond(i)
+        master = input('>')
+        print(master)        
+        bot.entertain(master)
+    
+
+if __name__ == '__main__':
+    wake_the_bot()
